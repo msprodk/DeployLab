74247403404e05af74f002d256cfa206efabe305
Per Pedersen <per@mspro.dk>
2021-02-07 16:42:17 +0100

    -

---
+++ DSC/PrepareADClient.ps1
@@ -0,0 +1,30 @@
+﻿configuration PrepareADBDC
+{
+   param
+    (
+        [Parameter(Mandatory)]
+        [String]$DNSServer,
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    )
+
+    Import-DscResource -ModuleName  xStorage, xNetworking
+    $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
+    $InterfaceAlias=$($Interface.Name)
+
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        xDnsServerAddress DnsServerAddress
+        {
+            Address        = $DNSServer
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+        }
+   }
+}
